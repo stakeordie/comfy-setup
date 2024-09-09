3c0a2f583b120cb02e5393acac5acac0432c7ba4
Luis Espinoza <79593471+luiseel@users.noreply.github.com>
2024-09-09 11:47:58 -0700

    Another approach to force sourcing

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bash -i
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 # Force .bashrc
-exec bash
+source ~/.bashrc
 nvm install stable
 npm install -g pm2
