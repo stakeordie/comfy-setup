b9a5b23512c462b190b7dbc100caf6e4b93297e9
Luis Espinoza <79593471+luiseel@users.noreply.github.com>
2024-09-09 11:53:43 -0700

    Add README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# comfy-setup
+
+## Run
+
+Clone the repo, cd into the directory, and run the following command:
+
+```bash
+./init.sh && source ~/.bashrc
+```
--- init.sh
+++ init.sh
@@ -4,4 +4,3 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 source ~/.bashrc
 nvm install stable
 npm install -g pm2
-source ~/.bashrc
