7b35ad3bc91f18461198c0ba4b8e2e2e9aa0494d
Luis Espinoza <79593471+luiseel@users.noreply.github.com>
2024-09-09 11:50:42 -0700

    Final test

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -i
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-# Force .bashrc
 source ~/.bashrc
 nvm install stable
 npm install -g pm2
+source ~/.bashrc
