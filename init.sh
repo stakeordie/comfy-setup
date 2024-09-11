dc413bb624eac5d6440d52b62763a699e2ae56fa
Sandy Corsillo <the-dusky@users.noreply.github.com>
2024-09-11 11:35:32 -0400

    Update init.sh
    added nano

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -i
-
+apt update
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 source ~/.bashrc
 nvm install stable
 npm install -g pm2
+apt-get install nano
