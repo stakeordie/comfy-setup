24dbc12794936ac98989524f8094a9dd12636f02
Sandy Corsillo <the-dusky@users.noreply.github.com>
2024-09-11 17:52:49 -0400

    added full comfy setup

--- init.sh
+++ init.sh
@@ -4,4 +4,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 source ~/.bashrc
 nvm install stable
 npm install -g pm2
-apt-get install nano gdown
+apt-get install nano
+pip install gdown
+cd /workspace && git clone https://github.com/comfyanonymous/ComfyUI.git
+cd ComfyUI && git reset --hard 9f4daca
+pip install -r requirements.txt
+pm2 start --name comfy "python3 main.py --port 3000 --listen"
