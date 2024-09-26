43cfe308a2371784ecb6e3b9e88ae59b91f06b1b
Sandy Corsillo <the_dusky@icloud.com>
2024-09-25 23:11:59 -0400

    update init refactor

--- init.sh
+++ init.sh
@@ -3,8 +3,7 @@ apt update
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 source ~/.bashrc
 nvm install stable && npm install -g pm2
-apt-get install nano
-apt-get update && apt-get install ffmpeg libsm6 libxext6 -y
+apt-get update && apt-get install nano ffmpeg libsm6 libxext6 -y
 cd ~
 git clone https://github.com/comfyanonymous/ComfyUI.git
 cd ComfyUI && git reset --hard 9f4daca
